158e870314261fbcc77e6f1105fe6e29fc65e48d
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2023-12-13 09:25:38 -0800

    ui ui

--- src/lib/actions-components/SuggestName.svelte
+++ src/lib/actions-components/SuggestName.svelte
@@ -7,37 +7,53 @@
   let requestState: "idle" | "loading" = "idle";
 </script>
 
-<button
-  class="underline"
-  on:click={() => {
-    requestState = "loading";
-    suggestNameForPalette($store.currentPal)
-      .then((x) => {
-        nameSuggestions = x;
-        requestState = "idle";
-      })
-      .catch((e) => {
-        console.log(e);
-        requestState = "idle";
-      });
-  }}
->
-  Suggest a name {#if requestState === "loading"}(loading){/if}
-</button>
-<div class="flex flex-wrap items-start">
-  {#each nameSuggestions as name}
-    <button
-      class="ml-2 border-2 rounded border-black p-2"
-      on:click={() => store.setMostRecentPal(name)}
-    >
-      ۞{name}
-    </button>
-  {/each}
-</div>
-<!-- <button
+<div class="border-2 border-white rounded p-2">
+  <button
+    class="bg-amber-400 text-white rounded p-2"
+    on:click={() => {
+      if (requestState === "loading") return;
+      requestState = "loading";
+      suggestNameForPalette($store.currentPal)
+        .then((x) => {
+          nameSuggestions = x;
+          requestState = "idle";
+        })
+        .catch((e) => {
+          console.log(e);
+          requestState = "idle";
+        });
+    }}
+  >
+    Suggest a name
+    {#if requestState === "loading"}(loading)
+    {/if}
+  </button>
+
+  <div class="flex flex-wrap items-start">
+    {#each nameSuggestions as name}
+      <div class="ml-2 rounded bg-green-800 text-white">
+        <button
+          class="rounded p-2"
+          on:click={() => store.setMostRecentPal(name)}
+        >
+          {name}
+        </button>
+        <button
+          on:click={() => {
+            nameSuggestions = nameSuggestions.filter((x) => x !== name);
+          }}
+          class="rounded p-2 bg-amber-700"
+        >
+          X
+        </button>
+      </div>
+    {/each}
+  </div>
+  <!-- <button
   on:click={() => {
     nameSuggestions = ["red", "blue", "green"];
   }}
 >
   fake it
 </button> -->
+</div>

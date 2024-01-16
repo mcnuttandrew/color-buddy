5373379bbe9c33eab2f2c01c53edea244c38d7aa
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2024-01-16 12:36:36 -0800

    .

--- src/components/Vega.svelte
+++ src/components/Vega.svelte
@@ -3,23 +3,13 @@
   import Example from "./Example.svelte";
   import { getSVG } from "../lib/charts";
 
-  import { idxToKey } from "../lib/charts";
   export let spec: any;
 
   let producedSVG: string = "";
-  $: colors = $colorStore.currentPal.colors;
 
   $: getSVG(spec, $colorStore.currentPal).then((x) => {
     producedSVG = x;
   });
-  // $: bg = $colorStore.currentPal.background.toHex();
-  // $: finalSVG = colors.reduce(
-  //   (acc, color, idx) => {
-  //     return acc.replace(new RegExp(idxToKey(idx), "g"), color.toHex());
-  //   },
-  //   producedSVG.replace("SaLmOn", bg)
-  // );
 </script>
 
-<!-- <div class="flex max-w-fit">{@html finalSVG}</div> -->
 <Example example={producedSVG} />

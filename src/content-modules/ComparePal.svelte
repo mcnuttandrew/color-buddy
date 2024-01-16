1b059c856359d774ac6c634448099ea6d6eb0b5d
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2024-01-16 15:17:46 -0800

    light weight comparison demo

--- src/content-modules/ComparePal.svelte
+++ src/content-modules/ComparePal.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
+  import { colorFromHex, colorFromString } from "../lib/Color";
   import colorStore from "../stores/color-store";
   import configStore from "../stores/config-store";
+  import focusStore from "../stores/focus-store";
   import ColorScatterPlot from "../components/ColorScatterPlot.svelte";
 
   $: ComparisonPal = $colorStore.palettes.find(
     (x) => x.name === $configStore.comparePal
   );
-  $: bg = ComparisonPal?.background.toHex() || "white";
-  $: colorSpace = ComparisonPal?.colorSpace || "lab";
+  $: {
+    if (
+      ComparisonPal === undefined &&
+      $colorStore.currentPal.name === $configStore.comparePal
+    ) {
+      ComparisonPal = $colorStore.currentPal;
+    }
+  }
+  let bg = ComparisonPal?.background.toHex() || "white";
+
+  let colorSpace = ComparisonPal?.colorSpace || "lab";
 </script>
 
 <div class="flex flex-col" style={`background: ${bg}`}>
   <select value={$configStore.comparePal}>
-    {#each $colorStore.palettes as pal}
+    {#each [$colorStore.currentPal, ...$colorStore.palettes] as pal}
       <option
         value={pal.name}
         on:click={() => {
@@ -26,9 +37,9 @@
   {#if ComparisonPal !== undefined}
     <ColorScatterPlot
       scatterPlotMode="looking"
-      Pal={ComparisonPal}
+      Pal={{ ...ComparisonPal, background: colorFromHex(bg, colorSpace) }}
       {colorSpace}
-      focusedColors={[]}
+      focusedColors={$focusStore.focusedColors}
       height={450}
       width={450}
       onColorsChange={() => {}}
@@ -37,4 +48,20 @@
       stopDragging={() => {}}
     />
   {/if}
+</div>
+<div class="bg-white">
+  <label>
+    Color Space:
+    <select bind:value={colorSpace}>
+      {#each ["lab", "oklab", "hsl", "hsv", "jzazbz"] as space}
+        <option value={space}>
+          {space}
+        </option>
+      {/each}
+    </select>
+  </label>
+  <label>
+    Background:
+    <input type="color" bind:value={bg} />
+  </label>
 </div>

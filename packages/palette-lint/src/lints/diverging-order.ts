df8ea09ca5948bd8bcc5449649e00312a36bac73
Andrew McNutt <mcnutt.andrew@gmail.com>
2024-06-10 16:22:34 -0700

    Improve diverging fix (#79)
    * Improve diverging fix
    
    * no only

--- packages/palette-lint/src/lints/diverging-order.ts
+++ packages/palette-lint/src/lints/diverging-order.ts
@@ -3,23 +3,19 @@ import type { PalType, ColorWrap } from "../types";
 import { Color } from "../Color";
 import type { LintFixer } from "../linter-tools/lint-fixer";
 
-// @ts-ignore
-const meanPoint2d = (points: Color[]) => {
-  const labPoints = points.map((x) => x.toColorIO().to("lab").coords);
-  const xs = labPoints.map((x) => x[1]);
-  const ys = labPoints.map((x) => x[2]);
-  const x = xs.reduce((a, b) => a + b, 0) / xs.length;
-  const y = ys.reduce((a, b) => a + b, 0) / ys.length;
-  return { x, y };
-};
-// @ts-ignore
-const findMinDistPoint = (points: Color[], pos: { x: number; y: number }) => {
-  const labPoints = points.map((x) => x.toColorIO().to("lab").coords);
-  const { x, y } = pos;
-  const distances = labPoints.map(([x1, y1]) => Math.hypot(x1 - x, y1 - y));
-  const minDist = Math.min(...distances);
-  return points[distances.indexOf(minDist)];
-};
+function isDiverging(colors: ColorWrap<Color>[]): boolean {
+  const summarizedDirections = colors
+    .slice(0, -1)
+    .map((x, i) =>
+      x.color.luminance() > colors[i + 1].color.luminance() ? 1 : -1
+    )
+    .reduce((acc, x) => {
+      if (acc.length === 0) return [x];
+      if (acc[acc.length - 1] === x) return acc;
+      return [...acc, x];
+    }, [] as number[]);
+  return summarizedDirections.length === 2;
+}
 export default class DivergingOrder extends ColorLint<boolean> {
   name = "Diverging Palettes order";
   taskTypes = ["diverging"] as PalType[];
@@ -27,23 +23,11 @@ export default class DivergingOrder extends ColorLint<boolean> {
   requiredTags = [];
   description: string = `Diverging palettes should have a middle color that is the lightest or darkest color. This is because if they are not, then they will not be differentiable from each other in some contexts.`;
   _runCheck() {
-    const { colors } = this.palette;
-    if (colors.length <= 2) {
+    if (this.palette.colors.length <= 2) {
       return { passCheck: true, data: false };
     }
 
-    const summarizedDirections = colors
-      .slice(0, -1)
-      .map((x, i) =>
-        x.color.luminance() > colors[i + 1].color.luminance() ? 1 : -1
-      )
-      .reduce((acc, x) => {
-        if (acc.length === 0) return [x];
-        if (acc[acc.length - 1] === x) return acc;
-        return [...acc, x];
-      }, [] as number[]);
-
-    return { passCheck: summarizedDirections.length === 2, data: false };
+    return { passCheck: isDiverging(this.palette.colors), data: false };
   }
   buildMessage(): string {
     return `This palette should have a middle color that is the lightest or darkest color, from which the other colors grow darker or lighter  respectively.`;
@@ -51,48 +35,47 @@ export default class DivergingOrder extends ColorLint<boolean> {
   subscribedFix: string = "fixDivergingOrder";
 }
 
-export const fixDivergingOrder: LintFixer = async (palette) => {
-  // figure out if its centered on a light color or a dark color?
-  // a dumb hueristic is just look at what the center color is in lab space, and see if its darker or lighter than most colors
+// const sortByLum = (a: ColorWrap<Color>, b: ColorWrap<Color>) => {
+//   const aL = a.color.luminance();
+//   const bL = b.color.luminance();
+//   if (aL === bL) return 0;
+//   return aL > bL ? 1 : -1;
+// };
 
-  let colors = [...palette.colors];
-  // const medianPoint = findMinDistPoint(colors, meanPoint2d(colors));
-  // console.log(medianPoint.toHex());
-  // let darkerThanMedian = colors.filter(
-  //   (x) => x.luminance() < medianPoint.luminance()
-  // ).length;
+// https://stackoverflow.com/questions/9960908/permutations-in-javascript
+const permutator = <T>(inputArr: T[]): T[][] => {
+  let result = [] as T[][];
 
-  const sortByLum = (a: ColorWrap<Color>, b: ColorWrap<Color>) => {
-    const aL = a.color.luminance();
-    const bL = b.color.luminance();
-    if (aL === bL) return 0;
-    return aL > bL ? 1 : -1;
-  };
-  // if (darkerThanMedian < colors.length / 2) {
-  //   console.log("reversing");
-  //   colors = colors.reverse();
-  // }
-
-  // const lightPoint = colors.at(-1)!;
-  const leftColors = [colors.at(-1)!];
-  const rightColors = [colors.at(-2)!];
-  for (let i = 0; i < colors.length - 2; i++) {
-    const color = colors[i];
-    const leftColor = leftColors.at(-1)!;
-    const rightColor = rightColors.at(-1)!;
-    if (
-      color.color.deltaE(leftColor.color) < color.color.deltaE(rightColor.color)
-    ) {
-      leftColors.push(color);
+  const permute = (arr: T[], m: T[] = []) => {
+    if (arr.length === 0) {
+      result.push(m);
     } else {
-      rightColors.push(color);
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
     }
+  };
+
+  permute(inputArr);
+
+  return result;
+};
+
+export const fixDivergingOrder: LintFixer = async (palette) => {
+  // dumb heuristic: try out all possible orders and see which ones pass the check, pick one
+  const colors = [...palette.colors];
+  const n = colors.length;
+  const allOrders = permutator(Array.from({ length: n }, (_, j) => j)).map(
+    (x) => x.map((i) => colors[i])
+  );
+  const passingOrders = allOrders.filter((order) => isDiverging(order));
+  if (passingOrders.length === 0) {
+    return [];
   }
-  colors = [
-    ...leftColors.sort(sortByLum),
-    ...rightColors.sort(sortByLum).reverse(),
-  ];
-  return [{ ...palette, colors }];
+  const newColors = passingOrders[0];
+  return [{ ...palette, colors: newColors }];
 };
 // {
 //     "exist": {
@@ -127,3 +110,57 @@ export const fixDivergingOrder: LintFixer = async (palette) => {
 //         }
 //     }
 // }
+
+// // @ts-ignore
+// const meanPoint2d = (points: Color[]) => {
+//   const labPoints = points.map((x) => x.toColorIO().to("lab").coords);
+//   const xs = labPoints.map((x) => x[1]);
+//   const ys = labPoints.map((x) => x[2]);
+//   const x = xs.reduce((a, b) => a + b, 0) / xs.length;
+//   const y = ys.reduce((a, b) => a + b, 0) / ys.length;
+//   return { x, y };
+// };
+// // @ts-ignore
+// const findMinDistPoint = (points: Color[], pos: { x: number; y: number }) => {
+//   const labPoints = points.map((x) => x.toColorIO().to("lab").coords);
+//   const { x, y } = pos;
+//   const distances = labPoints.map(([x1, y1]) => Math.hypot(x1 - x, y1 - y));
+//   const minDist = Math.min(...distances);
+//   return points[distances.indexOf(minDist)];
+// };
+
+// // figure out if its centered on a light color or a dark color?
+// // a dumb heuristic is just look at what the center color is in lab space, and see if its darker or lighter than most colors
+
+// let colors = [...palette.colors].sort(sortByLum);
+// // const medianPoint = findMinDistPoint(colors, meanPoint2d(colors));
+// // console.log(medianPoint.toHex());
+// // let darkerThanMedian = colors.filter(
+// //   (x) => x.luminance() < medianPoint.luminance()
+// // ).length;
+
+// // if (darkerThanMedian < colors.length / 2) {
+// //   console.log("reversing");
+// //   colors = colors.reverse();
+// // }
+
+// // const lightPoint = colors.at(-1)!;
+// const leftColors = [colors.at(-1)!];
+// const rightColors = [colors.at(-2)!];
+// for (let i = 0; i < colors.length - 2; i++) {
+//   const color = colors[i];
+//   const leftColor = leftColors.at(-1)!;
+//   const rightColor = rightColors.at(-1)!;
+//   if (
+//     color.color.deltaE(leftColor.color) < color.color.deltaE(rightColor.color)
+//   ) {
+//     leftColors.push(color);
+//   } else {
+//     rightColors.push(color);
+//   }
+// }
+// colors = [
+//   ...leftColors.sort(sortByLum),
+//   ...rightColors.sort(sortByLum).reverse(),
+// ];
+// return [{ ...palette, colors }];
